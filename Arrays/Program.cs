d557c55c2ed84d5a7b362d5f97a4b23b133f5ea1
SebasAKD <racingymotos@gmail.com>
2019-05-14 21:14:06 -0300

    Clase 4, ejemplo y práctica de arrays.

---
+++ Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("Ingrese la longitud del array:");
+            i = Int32.Parse(Console.ReadLine());
+            int [] numbers = new int [i];
+            i = 0;
+            Console.WriteLine("Ahora ingrese los {0} números:", numbers.Length);
+            while(i < numbers.Length)
+            {
+                if(Int32.TryParse(Console.ReadLine(), out numbers[i]))
+                {
+                    i++;
+                }else
+                {
+                    Console.WriteLine("Ese no es un número, intente de nuevo.");
+                }
+            }
+            i=0;
+            while(i<numbers.Length)
+            {
+                Console.WriteLine("Valor del elemento {0} : {1}", i, numbers[i]);
+                i++;
+            }
+        }
+    }
+}
