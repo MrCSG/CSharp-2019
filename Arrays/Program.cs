1136e417c9f27f7d1c667e1b9bd9a4af84fe1c51
SebasAKD <racingymotos@gmail.com>
2019-05-14 21:52:44 -0300

    Ejercicio 18 realizado en clase.

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,14 +6,13 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            int i;
             Console.WriteLine("Ingrese la longitud del array:");
-            i = Int32.Parse(Console.ReadLine());
+            int i = Int32.Parse(Console.ReadLine()), lookFor, quantity = 0;
             int [] numbers = new int [i];
             i = 0;
-            Console.WriteLine("Ahora ingrese los {0} números:", numbers.Length);
             while(i < numbers.Length)
             {
+                Console.Write("Ingrese el valor del elemento {0} :", i);
                 if(Int32.TryParse(Console.ReadLine(), out numbers[i]))
                 {
                     i++;
@@ -22,12 +21,18 @@ namespace Arrays
                     Console.WriteLine("Ese no es un número, intente de nuevo.");
                 }
             }
-            i=0;
+            Console.WriteLine("Qué número desea buscar en este array?");
+            lookFor = Int32.Parse(Console.ReadLine());
+            i = 0;
             while(i<numbers.Length)
             {
-                Console.WriteLine("Valor del elemento {0} : {1}", i, numbers[i]);
+                if (numbers[i] == lookFor)
+                {
+                    quantity ++;
+                }
                 i++;
             }
+            Console.WriteLine($"El número {lookFor} aparece {quantity} veces en el array");
         }
     }
 }
